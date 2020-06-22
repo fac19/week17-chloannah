498b86be01c90641d8b008fd2436a6d2d63b9bf5
Chloe <chloeh24@hotmail.com>
2020-06-22 14:02:51 +0100

    update firebase setup
    Co-authored-by: Hannah Gooding <hannah.gooding@yahoo.com>

---
+++ src/components/firebase/Firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+import React from "react";
+
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+const Firebase = () => {
+  firebase.initializeApp(config);
+};
+
+const FirebaseContext = React.createContext(null);
+
+export default Firebase;
+
+export { FirebaseContext };
